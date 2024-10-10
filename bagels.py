b9313a3d54ff3d8c126caf1dc2cbe8b3e7cfe633
John Michael Jarvis <97367692+JMJarvis1@users.noreply.github.com>
2024-10-10 19:41:24 +0000

    init commit

---
+++ bagels.py
@@ -0,0 +1,21 @@
+"""Bagels - A deductive reasoning game where the player is asked to guess a number of a set length using the clues provided following each incorrect guess. The player wins if they guess correctly before running out of guesses"""
+
+MAX_GUESSES: int = 10 # Number of guesses available to the player
+NUM_DIGITS: int = 3 # Length of the 'secret number'
+
+def main() -> None:
+    # TODO Print Instructions to the player
+    
+    # TODO Get secret number
+
+    # TODO Get player guess
+    
+    # TODO Get Clues 
+
+    # TODO Display clues or victor message
+    
+    # TODO Ask if player wants to play again   
+
+
+if __name__ == "__main__":
+    main()
